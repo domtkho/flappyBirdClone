436e754205c487f016d4e1d9155ca1647effe3e2
Dominic Ho <domtkho@gmail.com>
2015-04-12 00:54:16 +0800

    Added destruction of damage label.

--- main.js
+++ main.js
@@ -76,9 +76,9 @@ var mainState ={
       return;
     }
     this.bird.alive = false;
-    game.time.event.remove(this.timer);
+    game.time.events.remove(this.timer);
     this.pipes.forEachAlive(function(p){
-      p.velocity.x = 0;
+      p.body.velocity.x = 0;
     }, this);
   },
 };
